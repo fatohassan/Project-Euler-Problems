3370ab2bdd854222e175f8b38cbff989bcc65dd2
fatohassan <126200177+fatohassan@users.noreply.github.com>
2023-02-25 12:31:32 +0200

    Update Multiples-of-3-or-5.py

--- Multiples-of-3-or-5.py
+++ Multiples-of-3-or-5.py
@@ -1,26 +1,20 @@
 # The sum of all the multiples of 3 or 5 below 1000
-# First the last number less than 1000 that can be divided by 3 is 999 so "999/3=333" so the range of the first for loop between (0,334)
-# Second // // // by 5 is 995 so "995/5=199" so the range of the second for loop between (0, 200)
+# First: the last number less than 1000 that can be divided by 3 is 999 so "999/3=333" so the range of the first for loop between (1,333)
+# Second: // // // by 5 is 995 so "995/5=199" so the range of the second for loop between (1, 199)
 
 Multiple_of_3 = []
-for i in range(0, 334):
-    z = 3
-    z = z * i
+for i in range(334):
+    z = 3 * i
     Multiple_of_3.append(z)
 
 Multiple_of_5 = []
-for j in range(0, 200):
-    x = 5
-    x = x * j
-    Multiple_of_5.append(x)
-
-# Put the multiples of 3 and 5 in sets
-set1 = set(Multiple_of_3)
-set2 = set(Multiple_of_5)
-
-# Take the union of sets to avoid duplicate multiples
-union = list(set1.union(set2))
+for j in range(200):
+    x = 5 * j
+    if x % 3 == 0:
+        continue
+    else:
+        Multiple_of_5.append(x)
 
 # The final answer
-print(sum(union))
+print(sum(Multiple_of_3) + sum(Multiple_of_5))
 
